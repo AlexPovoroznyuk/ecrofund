4288a4d9e5da2130a9e8b9dd04f897c40b353aba
Alex <lexesss64@gmail.com>
2019-12-22 02:22:09 +0200

    add header dropdowns, search + scripts

--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -1,1 +1,44 @@
+$(".search-trigger").click(function(){
+    $(".search-container").addClass("active");
+    $("header").addClass("search-mod");
+})
 
+$(".search-cancel").click(function(){
+    $(".search-container").removeClass("active");
+    $("header").removeClass("search-mod");
+})
+
+$(function($){
+    $(document).mouseup(function (e){
+        var block = $(".search-container");
+        if (!block.is(e.target) 
+            && block.has(e.target).length === 0) { 
+                $(".search-container").removeClass("active");
+                $("header").removeClass("search-mod");
+        }
+    });
+});
+
+$(".news-search").click(function(){
+    $(".news-search").addClass("hide");
+    $(".news-search-container").addClass("active");
+    $("body").addClass("modal-open");
+})
+
+$(".news-cancel-btn").click(function(){
+    $(".news-search").removeClass("hide");
+    $(".news-search-container").removeClass("active");
+    $("body").removeClass("modal-open");
+})
+
+$(function($){
+    $(document).mouseup(function (e){
+        var block = $(".news-search-container");
+        if (!block.is(e.target) 
+            && block.has(e.target).length === 0) { 
+                $(".news-search").removeClass("hide");
+                $(".news-search-container").removeClass("active");
+                $("body").removeClass("modal-open");
+        }
+    });
+});
